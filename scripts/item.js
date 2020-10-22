0b76983bb9b7dcfe033040c091fe9d4ed179fcdd
kittybagpipes <sparticus978@gmail.com>
2020-10-22 16:48:48 -0700

    added and tested two function validateName and create

--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,18 @@
-export default {};
+const validateName = function (name) {
+    if (name.length === 0) {
+        throw TypeError("name must not be blank")
+    }
+}
+
+const create = function (name) {
+  return {
+      id: cuid(), 
+      name: name,
+      checked: false
+    }
+}
+
+export default {
+    validateName,
+    create
+};
